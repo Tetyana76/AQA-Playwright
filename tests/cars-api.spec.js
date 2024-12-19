72a8d2ea4f54c04fa7a23e4462ff6159b561853c
Tetiana Klapchenko <tklapchenko@gmail.com>
2024-12-19 17:41:36 +0200

    homework28

---
+++ tests/cars-api.spec.js
@@ -0,0 +1,120 @@
+import { test, request as apiRequest, expect } from '@playwright/test';
+
+test.describe('API: create cars', () => {
+    let requestContext;
+    let headers;
+
+    test.beforeAll(async ({ browser }) => {
+        requestContext = await apiRequest.newContext();
+        const context = await browser.newContext({
+            httpCredentials: { username: 'guest', password: 'welcome2qauto', send: 'always' }
+        });
+        // const page = await context.newPage();
+        const cookies = await context.cookies();
+        const cookiesParsed = cookies.reduce((acc, curr) => `${acc}${curr.name}=${curr.value}; `, '');
+        headers = { cookies: cookiesParsed };
+    });
+
+    test('Add a new car successfully', async () => {
+        const carData = {
+            carBrandId: 1,
+            carModelId: 1,
+            mileage: 15000
+        };
+
+        const createCarResponse = await requestContext.post('https://qauto.forstudy.space/api/cars', {
+            headers,
+            data: carData
+        });
+        const respJson = await createCarResponse.json();
+        console.log('Created car:', respJson);
+
+        expect(createCarResponse.ok()).toBeTruthy();
+        expect(createCarResponse.status()).toEqual(201);
+        expect(respJson.data).toHaveProperty('id');
+        expect(respJson.data.carBrandId).toEqual(1);
+        expect(respJson.data.carModelId).toEqual(1);
+        expect(respJson.data.mileage).toEqual(15000);
+    });
+
+    test('404 Brand not found: invalid carBrandId', async () => {
+        const carData = {
+            carBrandId: 150, // invalid id
+            carModelId: 2, 
+            mileage: 12000 
+        };
+
+        const response = await requestContext.post('https://qauto.forstudy.space/api/cars', {
+            headers,
+            data: carData
+        });
+
+        const respJson = await response.json();
+        console.log('404 Response:', respJson);
+
+        expect(response.status()).toBe(404);
+        expect(respJson).toHaveProperty('status', 'error');
+        expect(respJson).toHaveProperty('message', 'Brand not found');
+    });
+
+     test('400 Invalid car model type: invalid id format', async () => {
+        const carData = {
+            carBrandId: 2, 
+            carModelId: 'invalid', // invalid format
+            mileage: 12000 
+        };
+
+        const response = await requestContext.post('https://qauto.forstudy.space/api/cars', {
+            headers,
+            data: carData
+        });
+
+        const respJson = await response.json();
+        console.log('400 Response:', respJson);
+
+        expect(response.status()).toBe(400);
+        expect(respJson).toHaveProperty('status', 'error');
+        expect(respJson).toHaveProperty('message', 'Invalid car model type');
+    });   
+
+    test('404 Not Found: invalid route', async () => {
+        const carData = {
+            carBrandId: 1,
+            carModelId: 1,
+            mileage: 15000
+        };
+
+        const response = await requestContext.post('https://qauto.forstudy.space/api/cars-new', {
+            headers,
+            data: carData
+        });
+
+        const respJson = await response.json();
+        console.log('404 Response:', respJson);
+
+        expect(response.status()).toBe(404);
+        expect(respJson).toHaveProperty('status', 'error');
+        expect(respJson).toHaveProperty('message', 'Not found');
+    });
+    test('400 Bad Request: data is incomplete', async () => {
+        const carData = {
+            carBrandId: 1, // not all required data
+            mileage: 15000      
+        };
+
+        const response = await requestContext.post('https://qauto.forstudy.space/api/cars', {
+            headers,
+            data: carData
+        });
+
+        const respJson = await response.json();
+        console.log('400 Response (Incomplete Data):', respJson);
+
+        expect(response.status()).toBe(400); 
+        expect(respJson).toHaveProperty('status', 'error');
+        expect(respJson).toHaveProperty('message', 'Car model id is required');
+    });
+    test.afterAll(async () => {
+        await requestContext.dispose();
+    });
+});
---
+++ tests/profile.mock.spec.js
@@ -0,0 +1,38 @@
+import { test, expect } from '@playwright/test';
+
+test('Profile: Fetch and replace original response', async ({ browser }) => {
+    const context = await browser.newContext({ httpCredentials: { username: 'guest', password: 'welcome2qauto', send: 'always' } })
+    const page = await context.newPage();
+    
+    await page.route('**/api/users/profile', async (route, request) => {
+      console.log('request.method()', request.method())
+      if (request.method() === 'GET') {
+        const response = await route.fetch();
+        const json = await response.json();
+        json.data = {
+          userId: 159705,
+          photoFilename: "default-user.png",
+          name: "Maria",
+          lastName: "Petrenko"
+        };
+        
+        await route.fulfill({
+          response,
+          json
+        });
+        console.log('Modified response JSON:', json);
+      } else {
+        await route.continue()
+      };
+    }
+ 
+  );
+
+    await page.goto('/panel/profile', { timeout: 60000 });
+    await page.pause();
+    const displayedFullName = await page.locator('.profile_name.display-4').textContent();
+    console.log('Displayed full name:', displayedFullName); 
+    expect(displayedFullName).toBe('Maria Petrenko');
+
+});
+
